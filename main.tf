8859f62961ecfed33dc9fd43f4f716c76be88a39
dilanasanga <dilanasanga.x@gmail.com>
2024-04-09 06:00:30 -0700

    open port 8080

--- main.tf
+++ main.tf
@@ -139,8 +139,8 @@ resource "aws_security_group" "dilan_security_group_01" {
   }
   ingress {
     description = "access to port 80"
-    from_port = "80"
-    to_port = "80"
+    from_port = "8080"
+    to_port = "8080"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
